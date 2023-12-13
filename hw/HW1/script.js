1884330d9b26f2eeba10d5a8e03490d5945c5595
cristinajucova <66320295+cristinajucova@users.noreply.github.com>
2023-12-13 11:12:56 +0200

    Create script.js

---
+++ hw/HW1/script.js
@@ -0,0 +1,18 @@
+a = parseInt(prompt("Enter \"A\" value"));
+b = parseInt(prompt("Enter \"B\" value"));
+
+
+
+
+document.write(
+    "<div class='myDiv'><h3>######## VALUES ########</h3>" + 
+    "<p> a = " + a + "</p>" +
+    "<p> b = " + b + "</p>" +
+    "<h3>###### OPERATIONS #####</h3>" +
+    "<p>a * b = " + (a * b) + "</p>" +
+    "<p>a / b = " + (a / b) + "</p>" +
+    "<p>a + b = " + (a + b) + "</p>" +
+    "<p>a - b = " + (a - b) + "</p>" +
+    "<h3>#######################</h3></div>"
+
+)
