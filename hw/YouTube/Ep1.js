23faab0af5ad7b391d6aeedbbd3938642de3412e
cristinajucova <66320295+cristinajucova@users.noreply.github.com>
2024-01-10 10:46:18 +0200

    Create Ep1.js

---
+++ hw/YouTube/Ep1.js
@@ -0,0 +1,44 @@
+//var, let, const
+
+// let nume = 'Daniel';
+// console.log(nume);
+// nume = 'Anca';
+// console.log(nume);
+
+// let mesaj;
+
+// mesaj = 'salut';
+// console.log(mesaj);
+
+// //reguli de denumire a variabilelor
+// //1. litere, cifre, _, $
+// //2. numele nu poate incepe cu o cifra.
+
+// let nume1 = 'Ana';
+
+// //nume de variabila compus
+
+// let firstName = 'Cristi'; //camel case
+// let first_name = 'Daniel' //underscore
+// let FirstName = 'Anca' //pascal case
+
+
+//CONST
+// const nume = 'Ion';
+// console.log(nume);
+
+// nume = 'Petre';
+// console.log(nume);
+
+// const mesaj;
+// mesaj = 'salut';
+
+const numere=[1, 2, 3];
+console.log(numere);
+
+numere[0] = 25;
+numere.push(17);
+console.log(numere);
+
+numere = [3, 4, 5];
+
