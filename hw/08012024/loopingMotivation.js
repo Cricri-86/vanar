35f43cde0b189f6cdbbdea45f71c75641739be87
cristinajucova <66320295+cristinajucova@users.noreply.github.com>
2024-01-10 11:28:43 +0200

    Create loopingMotivation.js

---
+++ hw/08012024/loopingMotivation.js
@@ -0,0 +1,11 @@
+//DATA: number of likes to a profile "Mr. X"
+let likes = 1;
+
+while(likes < 20){
+    //one like
+    likes++; //increment
+    if(likes >= 10){
+        document.write('You have MANY likes!!!<br>');
+    }
+    document.write("Likes: " + likes + "<hr>")
+}
