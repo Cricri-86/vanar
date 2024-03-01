a5bcb6402df122862074207231ee91996be7eba5
cristinajucova <66320295+cristinajucova@users.noreply.github.com>
2024-03-01 10:40:23 +0200

    Create app.js

---
+++ lvl2/Feb 28/socials/app.js
@@ -0,0 +1,11 @@
+let user_1 = new User("johny", 20, true)
+
+let user_2 = new User("marry", 21, false)
+
+let user_3 = new User("pete", 22, true)
+
+let message_1 = new Message("Hi, how are you?", "sent", user_1, user_2)
+
+let message_2 = new Message("I'm fine, thank you", "sent", user_2, user_1)
+
+let message_3 = new Message("Hi, how are you?", "sent", user_1, user_3)
