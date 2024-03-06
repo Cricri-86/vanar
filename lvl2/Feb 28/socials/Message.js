a1ba922ab010d907d470635687e117aa93e38c4e
cristinajucova <66320295+cristinajucova@users.noreply.github.com>
2024-03-06 12:29:59 +0200

    Update Message.js

--- lvl2/Feb 28/socials/Message.js
+++ lvl2/Feb 28/socials/Message.js
@@ -2,17 +2,18 @@ class Message {
 
     static all = []
 
-    constructor(content, status, from_user, to_user) {
-        this.content = content
-        this.status = status
-        this.from_user = from_user
-        this.to_user = to_user
+    constructor(content, fromUser, toUser, status) {
 
+        this.content = content
+        this.fromUser = fromUser
+        this.toUser = toUser
+        this.status = status
         Message.all.push(this)
+
     }
     render() {
         console.log(`
-        [${this.from_user.name}] has sent a new message to [${this.to_user.name}]
+        [${this.fromUser.name}] has sent a new message to [${this.toUser.name}]
         "${this.content}"
         Message status: ${this.status}
         `)
