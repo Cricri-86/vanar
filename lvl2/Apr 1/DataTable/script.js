bd39367d327fe55e3062df490a0664633d31451b
cristinajucova <66320295+cristinajucova@users.noreply.github.com>
2024-04-01 11:10:43 +0300

    Update script.js

--- lvl2/Apr 1/DataTable/script.js
+++ lvl2/Apr 1/DataTable/script.js
@@ -1,8 +1,43 @@
+const search = (e, data) => {
+    let phrase = e.target.value
+    data.employees = data.employees.filter(employee => {
+        return employee.name.startsWith(phrase)
+    })
+    //render(content, data)
+   
+}
+
 const render = (parentElement, data) => {
     const table = document.createElement('table')
     let tr
     let td
 
+    //column headers
+    tr = document.createElement('tr')
+    data.fields.forEach( key =>{
+        td = document.createElement('th')
+        td.innerText = key
+        tr.append(td)
+
+    })
+    table.append(tr)
+
+    // search form
+    tr = document.createElement('tr')
+    td = document.createElement('td')
+    td.colSpan = 6
+
+    let form = document.createElement('form')
+    let input = document.createElement('input')
+    input.placeholder = 'Search here...'
+    input.addEventListener('keyup', (e) => search(e, data))
+    form.append(input)
+    td.append(form)
+    tr.append(td)
+
+    table.append(tr)
+
+    //employee rows
     data.employees.forEach(
         employee => {
 
