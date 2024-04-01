748a6526c2315081aa97e3b5a7882fdddc4a21a5
cristinajucova <66320295+cristinajucova@users.noreply.github.com>
2024-04-01 10:37:49 +0300

    Create data.js

---
+++ lvl2/Apr 1/DataTable/data.js
@@ -0,0 +1,37 @@
+const data = {
+    employees:[
+        {
+            name: "John Doe",
+            photo: "https://avataaars.io/?avatarStyle=Circle&topType=LongHairCurvy&accessoriesType=Wayfarers&hairColor=Auburn&facialHairType=BeardMajestic&facialHairColor=Auburn&clotheType=ShirtVNeck&clotheColor=Gray01&eyeType=Cry&eyebrowType=UpDown&mouthType=Serious&skinColor=Yellow",
+            job: "developer",
+            experience: 100,
+            salary: 110_000,
+            contacts: {
+                viber: "+123456",
+                email: "test@email.com"
+            }
+        },
+        {
+            name: "Marry Poppins",
+            photo: "https://avataaars.io/?avatarStyle=Circle&topType=LongHairStraight&accessoriesType=Blank&hairColor=BrownDark&facialHairType=Blank&clotheType=BlazerShirt&eyeType=Default&eyebrowType=Default&mouthType=Default&skinColor=Light",
+            job: "tester",
+            experience: 80,
+            salary: 100_000,
+            contacts: {
+                whatsapp: "+789456",
+                phone: "+373456879"
+            }
+        },
+        {
+            name: "Pete Cringe",
+            photo: "https://avataaars.io/?avatarStyle=Circle&topType=LongHairStraight2&accessoriesType=Blank&hairColor=Blonde&facialHairType=MoustacheMagnum&facialHairColor=BrownDark&clotheType=Hoodie&clotheColor=Gray01&eyeType=Surprised&eyebrowType=Default&mouthType=Concerned&skinColor=Tanned",
+            job: "manager",
+            experience: 90,
+            salary: 150_000,
+            contacts: {
+                insta: "@petecringe",
+                email: "petecringe@email.com"
+            }
+        }
+    ]
+}
