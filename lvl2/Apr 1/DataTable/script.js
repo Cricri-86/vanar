6a770efd1aa552ab6d68d4f800ec55513f4cb752
cristinajucova <66320295+cristinajucova@users.noreply.github.com>
2024-04-03 14:22:42 +0300

    Update script.js

--- lvl2/Apr 1/DataTable/script.js
+++ lvl2/Apr 1/DataTable/script.js
@@ -1,41 +1,69 @@
+
+const renderCache = {
+
+}
+
+
 const search = (e, data) => {
     let phrase = e.target.value
+ 
     data.employees = data.employees.filter(employee => {
         return employee.name.startsWith(phrase)
     })
-    //render(content, data)
-   
+
+    render(content, data)
+
 }
 
 const render = (parentElement, data) => {
+    parentElement.innerHTML = ''
     const table = document.createElement('table')
     let tr
     let td
+    let input
 
     //column headers
-    tr = document.createElement('tr')
-    data.fields.forEach( key =>{
-        td = document.createElement('th')
-        td.innerText = key
-        tr.append(td)
+    if (renderCache.headers) {
+        tr = renderCache.headers
+    } else {
+        tr = document.createElement('tr')
+        data.fields.forEach(key => {
+            td = document.createElement('th')
+            td.innerText = key
+            tr.append(td)
 
-    })
+        })
+        renderCache.headers = tr
+    }
     table.append(tr)
+
 
     // search form
-    tr = document.createElement('tr')
-    td = document.createElement('td')
-    td.colSpan = 6
 
-    let form = document.createElement('form')
-    let input = document.createElement('input')
-    input.placeholder = 'Search here...'
-    input.addEventListener('keyup', (e) => search(e, data))
-    form.append(input)
-    td.append(form)
-    tr.append(td)
+    if (renderCache.searchForm) {
+        tr = renderCache.searchForm
+    } else {
+        tr = document.createElement('tr')
+        td = document.createElement('td')
+        td.colSpan = 6
+
+        let form = document.createElement('form')
+        let input = document.createElement('input')
+        input.placeholder = 'Search here...'
+        input.addEventListener('keyup', (e) => {search(e, data)})
+        form.append(input)
+        td.append(form)
+        tr.append(td)
+
+        renderCache.searchForm = tr
+    }
 
     table.append(tr)
+
+    input = tr.firstElementChild.firstElementChild.firstElementChild
+
+
+
 
     //employee rows
     data.employees.forEach(
@@ -94,6 +122,8 @@ const render = (parentElement, data) => {
         })
 
     parentElement.append(table)
+
+    input.focus()
 
 }
 
