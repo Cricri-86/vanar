a40daa8f862c6f33ebb3c59771c83da98f9abbf6
cristinajucova <66320295+cristinajucova@users.noreply.github.com>
2024-04-10 14:47:15 +0300

    Create script.js

---
+++ lvl2/Apr 10/Minichat/script.js
@@ -0,0 +1,61 @@
+let messages = []
+
+if (localStorage.getItem("messages")) {
+    messages = JSON.parse(localStorage.getItem("messages"))
+}
+
+const badWords = ["tomato", "hotdog"]
+
+const render = (messages) => {
+
+    let list = document.getElementById('messages-list')
+
+    list.innerHTML = ``
+
+    if (messages != null) {
+        messages.forEach(message => {
+            let li = document.createElement('li')
+
+            // preprocess render text
+            // String.replace
+            if (message.includes(':)')) {
+                message = message.replace(':)', '')
+                let image = document.createElement('img')
+                image.src = 'images/1-300x300.jpg'
+                li.innerText = message
+                li.appendChild(image)
+            } else {
+                li.innerText = message
+            }
+
+            list.appendChild(li)
+        })
+    }
+
+}
+
+const send = () => {
+    let input = document.getElementById('message-input')
+    let message = input.value.trim()
+    if (message != '') {
+
+        badWords.forEach(word => {
+            if (message.toLowerCase().includes(word)) {
+                message = '[censored]'
+            }
+        })
+
+        messages.unshift(message)
+
+        //// KEEPING ONLY 10
+        if (messages.length > 10) messages.pop()
+
+        ///// store data ///
+        localStorage.setItem("messages", JSON.stringify(messages))
+
+        input.value = ""
+    }
+}
+
+render(messages)
+
